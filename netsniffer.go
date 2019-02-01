cdcd7d924be60d4cabd0c7cc158ef48c6322f42c
adams003 <adamspouele@gmail.com>
2019-02-01 19:49:30 +0100

    initialized

---
+++ netsniffer.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Website Lookup CLI"
+	app.Usage = "Let's you query IPS, CNAMEs, MX records and Name  Servers !"
+
+	myFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "meshectares.com",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Looks up teh Name Servers for a Particular Host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "ip",
+			Usage: "Looks up the IP addresses for a Particular Host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ip, err := net.LookupIP(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(ip); i++ {
+					fmt.Println(ip[i])
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "cname",
+			Usage: "Looks up the CNAME for a Particular Host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				cname, err := net.LookupIP(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				fmt.Println(cname)
+				return nil
+			},
+		},
+		{
+			Name:  "mx",
+			Usage: "Looks up the MX records for a Particular Host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				mx, err := net.LookupMX(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(mx); i++ {
+					fmt.Println(mx[i].Host, mx[i].Pref)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
